7fde01222266d33020ccecfd71643689e31ff46b
LRegis <leonardocregis@gmail.com>
2016-08-10 16:01:29 -0300

    REfatoracao da gerencia do servidor para uma superclass

--- src/test/java/br/com/alura/loja/ProjetoResourceTest.java
+++ src/test/java/br/com/alura/loja/ProjetoResourceTest.java
@@ -6,31 +6,16 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import br.com.alura.loja.dao.ProjetoDAO;
 import br.com.alura.loja.modelo.Projeto;
 import br.com.alura.loja.resource.ProjetoResource;
 
-public class ProjetoResourceTest implements ServerConfigs {
+public class ProjetoResourceTest extends ServerTest implements ServerConfigs {
 
 
-	private Servidor servidor;
-
-	@Before
-	public void init (){
-		this.servidor = new Servidor();
-		this.servidor.start();
-	}
-	
-	@After
-	public void finish(){
-		this.servidor.stop();
-	}
-	
 	@Test
 	public void testaQueAConexaoComOServidorFunciona() {
 		Client client = ClientBuilder.newClient();
