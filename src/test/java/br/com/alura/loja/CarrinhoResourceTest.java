f72efbcce9ade10c7a9584f308f8512b51733947
LRegis <leonardocregis@gmail.com>
2016-08-10 16:20:00 -0300

    refatoracao para uso de id no teste por path

--- src/test/java/br/com/alura/loja/CarrinhoResourceTest.java
+++ src/test/java/br/com/alura/loja/CarrinhoResourceTest.java
@@ -21,8 +21,9 @@ public class CarrinhoResourceTest extends ServerTest {
 		WebTarget target = client.target("http://" + Servidor.SERVER + ":"+ Servidor.PORTA);
 		String path = CarrinhoResource.class.getAnnotation(Path.class).value();
 		CarrinhoDAO carrinhoDAO = new CarrinhoDAO();
-		Carrinho carrinho = carrinhoDAO.busca(1l);
-		String conteudo = target.path("/" + path).request().get(String.class);
+		long idCarrinho = 1l;
+		Carrinho carrinho = carrinhoDAO.busca(idCarrinho);
+		String conteudo = target.path("/" + path+"/"+idCarrinho).request().get(String.class);
 		System.out.println(conteudo);
 		Assert.assertTrue(conteudo.contains(carrinho.getRua()));
 
