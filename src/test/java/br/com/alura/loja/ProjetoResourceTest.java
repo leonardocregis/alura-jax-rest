1a951f25b21acdc0aac17b819fb78c292e94c37c
LRegis <leonardocregis@gmail.com>
2016-08-10 16:23:53 -0300

    Refatoracao para o uso de id no path parameter

--- src/test/java/br/com/alura/loja/ProjetoResourceTest.java
+++ src/test/java/br/com/alura/loja/ProjetoResourceTest.java
@@ -21,8 +21,9 @@ public class ProjetoResourceTest extends ServerTest  {
 		WebTarget target = client.target("http://"+Servidor.SERVER+":" +Servidor.PORTA);
 		String path = ProjetoResource.class.getAnnotation(Path.class).value();
 		ProjetoDAO projetoDAO = new ProjetoDAO();
-		Projeto p = projetoDAO.busca(1l);
-		String conteudo = target.path("/" + path).request().get(String.class);
+		long idProjeto = 1l;
+		Projeto p = projetoDAO.busca(idProjeto);
+		String conteudo = target.path("/" + path+"/"+idProjeto).request().get(String.class);
 		Assert.assertTrue(conteudo.contains(p.getNome()));
 	}
 
