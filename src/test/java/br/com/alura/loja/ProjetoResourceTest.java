af3f945ff42be6ff8dcf0fe9b4ec57f9f8bc57c1
LRegis <leonardocregis@gmail.com>
2016-08-10 15:10:52 -0300

    Refatoracao para adicao de @After e @Before para gerenciar o servidor

--- src/test/java/br/com/alura/loja/ProjetoResourceTest.java
+++ src/test/java/br/com/alura/loja/ProjetoResourceTest.java
@@ -5,7 +5,10 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.alura.loja.dao.ProjetoDAO;
@@ -14,6 +17,18 @@ import br.com.alura.loja.resource.ProjetoResource;
 
 public class ProjetoResourceTest implements ServerConfigs {
 
+	private HttpServer httpserver;
+
+	@Before
+	public void init (){
+		httpserver = new Servidor().createInstance();
+	}
+	
+	@After
+	public void finish(){
+		httpserver.stop();
+	}
+	
 	@Test
 	public void testaQueAConexaoComOServidorFunciona() {
 		Client client = ClientBuilder.newClient();
@@ -21,10 +36,8 @@ public class ProjetoResourceTest implements ServerConfigs {
 		String path = ProjetoResource.class.getAnnotation(Path.class).value();
 		ProjetoDAO projetoDAO = new ProjetoDAO();
 		Projeto p = projetoDAO.busca(1l);
-
 		String conteudo = target.path("/" + path).request().get(String.class);
 		Assert.assertTrue(conteudo.contains(p.getNome()));
-
 	}
 
 }
