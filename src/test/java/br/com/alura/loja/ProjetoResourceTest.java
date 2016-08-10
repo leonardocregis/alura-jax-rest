3f038bca0eb7fd9eafbf76558288aae5c5a5120d
LRegis <leonardocregis@gmail.com>
2016-08-10 14:50:28 -0300

    Criacao de Classe de Testes ProjetoResourceTeste

---
+++ src/test/java/br/com/alura/loja/ProjetoResourceTest.java
@@ -0,0 +1,30 @@
+package br.com.alura.loja;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.alura.loja.dao.ProjetoDAO;
+import br.com.alura.loja.modelo.Projeto;
+import br.com.alura.loja.resource.ProjetoResource;
+
+public class ProjetoResourceTest implements ServerConfigs {
+
+	@Test
+	public void testaQueAConexaoComOServidorFunciona() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:" + PORTA);
+		String path = ProjetoResource.class.getAnnotation(Path.class).value();
+		ProjetoDAO projetoDAO = new ProjetoDAO();
+		Projeto p = projetoDAO.busca(1l);
+
+		String conteudo = target.path("/" + path).request().get(String.class);
+		Assert.assertTrue(conteudo.contains(p.getNome()));
+
+	}
+
+}
