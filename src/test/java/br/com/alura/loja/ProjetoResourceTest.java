393a38fb211e1866d0a6b57ef252cb16c74eb97b
LRegis <leonardocregis@gmail.com>
2016-08-10 16:03:54 -0300

    simples remocao de imports nao usados

--- src/test/java/br/com/alura/loja/ProjetoResourceTest.java
+++ src/test/java/br/com/alura/loja/ProjetoResourceTest.java
@@ -5,7 +5,6 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
-import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.Assert;
 import org.junit.Test;
 
