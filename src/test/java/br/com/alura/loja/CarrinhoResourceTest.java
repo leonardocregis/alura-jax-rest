6511d1bc5e827133d92a60784d978b84b4fd724c
LRegis <leonardocregis@gmail.com>
2016-08-10 16:09:20 -0300

    Criacao de Teste para Carrinho de compras

---
+++ src/test/java/br/com/alura/loja/CarrinhoResourceTest.java
@@ -0,0 +1,30 @@
+package br.com.alura.loja;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.alura.loja.dao.CarrinhoDAO;
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.resource.CarrinhoResource;
+
+public class CarrinhoResourceTest extends ServerTest {
+
+	@Test
+	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
+
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://" + Servidor.SERVER + ":"+ Servidor.PORTA);
+		String path = CarrinhoResource.class.getAnnotation(Path.class).value();
+		CarrinhoDAO carrinhoDAO = new CarrinhoDAO();
+		Carrinho carrinho = carrinhoDAO.busca(1l);
+		String conteudo = target.path("/" + path).request().get(String.class);
+		System.out.println(conteudo);
+		Assert.assertTrue(conteudo.contains(carrinho.getRua()));
+
+	}
+}
