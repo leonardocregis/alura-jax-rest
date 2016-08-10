f0f4ce35cb812a912b2c45af365ac38aa6140d76
LRegis <leonardocregis@gmail.com>
2016-08-10 14:33:01 -0300

    Criando um projeto

---
+++ src/main/java/br/com/alura/loja/modelo/Projeto.java
@@ -0,0 +1,44 @@
+package br.com.alura.loja.modelo;
+
+import com.thoughtworks.xstream.XStream;
+
+public class Projeto implements Convertivel{
+	
+	private String nome;
+	long id;
+	int anoDeInicio;
+	
+	public Projeto(String nome, long id, int anoDeInicio) {
+		this.nome = nome;
+		this.id = id;
+		this.anoDeInicio = anoDeInicio;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public long getId() {
+		return id;
+	}
+	public int getAnoDeInicio() {
+		return anoDeInicio;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setAnoDeInicio(int anoDeInicio) {
+		this.anoDeInicio = anoDeInicio;
+	}
+
+	public String toXML() {
+		return new XStream().toXML(this);
+	}
+	
+	
+}
