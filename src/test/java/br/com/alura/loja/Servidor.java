fac3091717e68c69385c8305e107e1c92527aec4
LRegis <leonardocregis@gmail.com>
2016-08-10 15:01:21 -0300

    Refatoracao da Classe Servidor  - agora permite que ela crie uma instancia por si só , antes era tudo rodando no main
    Extração de Variáveis para ServerConfig

--- src/test/java/br/com/alura/loja/Servidor.java
+++ src/test/java/br/com/alura/loja/Servidor.java
@@ -7,15 +7,25 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-public class Servidor {
+public class Servidor implements ServerConfigs{
 
+	private URI uri;
+	private ResourceConfig config;
 
+	public Servidor(){
+		this.uri = URI.create("http://"+SERVER+":"+PORTA+"/");
+		this.config = new ResourceConfig().packages(PATH_TO_PACKAGES);
+	}
 
+	public HttpServer createInstance(){
+		System.out.println("Starting server = "+this.uri.toString());
+		return GrizzlyHttpServerFactory.createHttpServer(uri, config);
+	}
+	
 	public static void main(String[] args){
-	    URI uri = URI.create("http://localhost:8099/");
-	    ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
-		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
 		System.out.println("Servidor rodando");
+		Servidor servidor = new Servidor();
+		HttpServer server = servidor.createInstance();
 	    try {
 			System.in.read();
 		} catch (IOException e) {
