e7d07a98f4f0de2b901ba3ecb05dd494f5b6fda7
LRegis <leonardocregis@gmail.com>
2016-08-15 10:37:33 -0300

    Adicao de metodo Post que recebe um conteduo xml e insere no DAO

--- src/main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ src/main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -1,12 +1,18 @@
 package br.com.alura.loja.resource;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.dao.ProjetoDAO;
+import br.com.alura.loja.modelo.Projeto;
+
+import com.thoughtworks.xstream.XStream;
 
 
 @Path("projetos")
@@ -19,4 +25,13 @@ public class ProjetoResource {
 		return new ProjetoDAO().busca(id).toJSON();
 	}
 
+	
+	@POST
+	@Produces(MediaType.APPLICATION_XML)
+	@Consumes
+	public String adiciona(String conteudo){
+        Projeto carrinho = (Projeto) new XStream().fromXML(conteudo);
+        new ProjetoDAO().adiciona(carrinho);
+        return "<status>sucesso</status>";
+	}
 }
