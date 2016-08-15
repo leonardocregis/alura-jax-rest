025e1b7b9d8e31dc5831b3f3df5f9c1d6fbd8078
LRegis <leonardocregis@gmail.com>
2016-08-15 13:30:19 -0300

    refatoracao para carregar a path da resource de forma unificada.

--- src/test/java/br/com/alura/loja/CarrinhoResourceTest.java
+++ src/test/java/br/com/alura/loja/CarrinhoResourceTest.java
@@ -1,8 +1,9 @@
 package br.com.alura.loja;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.ws.rs.Path;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
@@ -13,23 +14,41 @@ import org.junit.Test;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
-import br.com.alura.loja.modelo.Projeto;
 import br.com.alura.loja.resource.CarrinhoResource;
-import br.com.alura.loja.resource.ProjetoResource;
 
 public class CarrinhoResourceTest extends ServerTest {
 
+	private String path;
+
+	private String getPathtoResource() {
+		if (this.path == null) {
+			this.path = getAnotationValue(CarrinhoResource.class);
+		}
+
+		return this.path;
+	}
 	@Test
 	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
 
 		WebTarget target = getTarget();
-		String path = CarrinhoResource.class.getAnnotation(Path.class).value();
+		
 		CarrinhoDAO carrinhoDAO = new CarrinhoDAO();
 		long idCarrinho = 1l;
 		Carrinho carrinho = carrinhoDAO.busca(idCarrinho);
-		String conteudo = target.path("/" + path+"/"+idCarrinho).request().get(String.class);
+		String conteudo = target.path("/" + getPathtoResource()+"/"+idCarrinho).request().get(String.class);
 		Assert.assertTrue(conteudo.contains(carrinho.getRua()));
 
+	}
+	
+	static String getAnotationValue(@SuppressWarnings("rawtypes") Class klass) {
+		Pattern p = Pattern.compile("value=(.*)\\)");
+		@SuppressWarnings("unchecked")
+		Matcher m = p.matcher(klass.getAnnotation(Path.class).toString());
+		String result= " not found";
+		if ( m.find()){
+			 result = m.group(1);
+		}
+		return result;
 	}
 	
 	@Test
@@ -39,7 +58,7 @@ public class CarrinhoResourceTest extends ServerTest {
 		
 		Carrinho carrinho = new Carrinho();
 		Entity<String> entity  = Entity.entity(carrinho.toXML(),MediaType.APPLICATION_XML);
-		Response response = getTarget().path("/"+path).request().post(entity);
+		Response response = getTarget().path("/"+getPathtoResource()).request().post(entity);
 		Assert.assertEquals("<status>sucesso</status>", response.readEntity(String.class));
 		
 	}
