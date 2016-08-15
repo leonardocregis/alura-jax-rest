0cd58479fcf61fdcb370f2c22119b27f56a97ddb
LRegis <leonardocregis@gmail.com>
2016-08-15 14:44:43 -0300

    Refatoracao para ao inserir fazer uma busca do id do projeto para validar se o processo está 100 correto

--- src/test/java/br/com/alura/loja/ProjetoResourceTest.java
+++ src/test/java/br/com/alura/loja/ProjetoResourceTest.java
@@ -1,5 +1,8 @@
 package br.com.alura.loja;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.ws.rs.Path;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
@@ -10,11 +13,28 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import br.com.alura.loja.dao.ProjetoDAO;
+import br.com.alura.loja.dao.ProjetoDAO;
+import br.com.alura.loja.modelo.Carrinho;
 import br.com.alura.loja.modelo.Projeto;
+import br.com.alura.loja.resource.CarrinhoResource;
 import br.com.alura.loja.resource.ProjetoResource;
 
 public class ProjetoResourceTest extends ServerTest  {
 
+
+	private static final String PROJETO_NOME = "Teste 1";
+	private String path;
+
+	static String getAnotationValue(@SuppressWarnings("rawtypes") Class klass) {
+		Pattern p = Pattern.compile("value=(.*)\\)");
+		@SuppressWarnings("unchecked")
+		Matcher m = p.matcher(klass.getAnnotation(Path.class).toString());
+		String result= " not found";
+		if ( m.find()){
+			 result = m.group(1);
+		}
+		return result;
+	}
 
 	@Test
 	public void testaQueAConexaoComOServidorFunciona() {
@@ -29,13 +49,36 @@ public class ProjetoResourceTest extends ServerTest  {
 
 	@Test
 	public void testAdicaoDeProjeto(){
-		//TODO adicionar teste para verificar ser realmente foi inserido o projeto, atualmente nao foi possivel pois teria que reorganizar o retorno trazendo o id do projeto inserido
-		String path = ProjetoResource.class.getAnnotation(Path.class).value();
+		Projeto projeto = new Projeto(PROJETO_NOME, 0, 2016);
 		
-		Projeto projeto = new Projeto("Teste1", 0, 2016);
 		Entity<String> entity  = Entity.entity(projeto.toXML(),MediaType.APPLICATION_XML);
-		Response response = getTarget().path("/"+path).request().post(entity);
-		Assert.assertEquals("<status>sucesso</status>", response.readEntity(String.class));
+		Response response = getTarget().path("/"+getPathtoResource()).request().post(entity);
+		Assert.assertEquals(response.getStatus(),201);
+		String stringResponse=  response.getHeaderString("Location");
+		long idProjeto =extraiIdProjeto(stringResponse);
+		projeto = new ProjetoDAO().busca(new Long(idProjeto));
+		String conteudo = getTarget().path("/" + getPathtoResource()+"/"+idProjeto).request().get(String.class);
+		Assert.assertTrue(conteudo.contains(PROJETO_NOME));
 		
+	}
+	
+
+	private String getPathtoResource() {
+		if (this.path == null) {
+			this.path = getAnotationValue(ProjetoResource.class);
+		}
+		return this.path;
+	}
+
+
+
+	private long extraiIdProjeto(String stringResponse) {
+		Pattern p = Pattern.compile("/(\\d)");
+		Matcher m = p.matcher(stringResponse);
+		long idProjeto = 0;
+		if (m.find()){
+			idProjeto = Long.parseLong(m.group(1));
+		}
+		return idProjeto;
 	}
 }
