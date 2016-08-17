a5170fa6ab5a13718f76cb0eb7979024f3b95779
LRegis <leonardocregis@gmail.com>
2016-08-17 10:28:16 -0300

    Refatoracao para uso de jaxb e adicao de exessao se nao existir um projeto

--- src/main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ src/main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -9,8 +9,10 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import br.com.alura.loja.dao.ProjetoDAO;
 import br.com.alura.loja.modelo.Projeto;
@@ -32,12 +34,12 @@ public class ProjetoResource {
 	@Path("{id}")
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public String busca(@PathParam("id") long id){
+	public Projeto busca(@PathParam("id") long id){
 		Projeto projeto = new ProjetoDAO().busca(id);
 		if (projeto !=null ){
-			return projeto.toXML();
+			return projeto;
 		}
-		return "";
+		throw new WebApplicationException(Status.NOT_FOUND);
 	}
 
 	
